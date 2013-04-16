dd893dbc11c7f1485f31f014fc8ffce290e4366a
KFOP <KFOP@Comp2>
2013-04-17 03:02:49 +0900

    freeglut libs

--- browse/TigerDisplay/main.c
+++ browse/TigerDisplay/main.c
@@ -1,33 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "Tiger.h"
-#include <GLUT/glut.h>
+#include "./Tiger.h"
+#include <GL/glut.h>
 
 static const char FILENAME[] = "Tiger.obj";
 
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);	// 설정된 배경색으로 화면 초기화
-    
-	float scale_factor = 1.0;	// 물체 크기 조절을 위한 확대 비율
+	glClear(GL_COLOR_BUFFER_BIT);	// �ㅼ젙��諛곌꼍�됱쑝濡��붾㈃ 珥덇린��
+	float scale_factor = 1.0;	// 臾쇱껜 �ш린 議곗젅���꾪븳 �뺣� 鍮꾩쑉
 	glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);	// 시점 설정
+    gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);	// �쒖젏 �ㅼ젙
     glPushMatrix();
     glScalef(scale_factor, scale_factor, scale_factor); //*/
 	
-    // Fill this area to render your objects…
-	//enable vertex coordinate array when glDrawArrays is called
+    // Fill this area to render your objects��	//enable vertex coordinate array when glDrawArrays is called
 	glEnableClientState(GL_VERTEX_ARRAY);
 	//enable normal array when glDrawArrays is called
 	glEnableClientState(GL_NORMAL_ARRAY);
 	//enable texture coordinate array when glDrawArrays is called
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	
+	// set input data to arrays
 	glVertexPointer(3, GL_FLOAT, 0, TigerVerts);
 	glNormalPointer(GL_FLOAT, 0, TigerNormals);
 	glTexCoordPointer(2, GL_FLOAT, 0, TigerTexCoords);
-	//
+
+	// draw data
 	glDrawArrays(GL_TRIANGLES, 0, TigerNumVerts);
 	
     glPopMatrix();
